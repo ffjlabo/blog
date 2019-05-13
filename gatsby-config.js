4127b3d5e9b187dc68d2d695c4249aa911cf1a5f
fujiwo <ffjlabo@gmail.com>
2019-05-13 14:56:20 +0900

    comment out code for images in gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,13 +7,13 @@ module.exports = {
   plugins: [
     `gatsby-plugin-styled-jsx`,
     `gatsby-plugin-react-helmet`,
-    {
+    /*{
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
-    },
+    },*/
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
