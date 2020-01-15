b9ef6b640690354b9013cf71139f6b055e4ac595
fujiwo <ffjlabo@gmail.com>
2020-01-15 16:54:07 +0900

    fix: separate header and footer components from layout

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React from "react"
+import styled from "styled-components"
+
+const HeaderContainer = styled.header`
+  text-align: center;
+`
+
+const Header = props => (
+  <HeaderContainer>
+    <h1>ふじを録</h1>
+  </HeaderContainer>
+)
+
+export default Header
