0d7d1051429278cd458d4e7793122ffc953660b6
fujiwo <ffjlabo@gmail.com>
2020-01-18 21:41:17 +0900

    add: global.js for global css style

---
+++ src/components/global.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components"
+
+const Global = createGlobalStyle`
+    body {
+        background-color: #F2F1F3;
+        margin: 0;
+    }
+`
+
+export default Global
