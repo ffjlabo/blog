bd570dd39cacc302cfe25dfb1a03d3fc4046b564
fujiwo <ffjlabo@gmail.com>
2020-01-19 17:00:35 +0900

    add: extended link component

---
+++ src/components/link.js
@@ -0,0 +1,10 @@
+import React from "react"
+import styled from "styled-components"
+import { Link } from "gatsby"
+
+const ExtendLink = styled(Link)`
+  text-decoration: none;
+  color: #000000;
+`
+
+export default ExtendLink
