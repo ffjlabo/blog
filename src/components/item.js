dcc992e59eae60fdaf79d093476859c057e72b5f
fujiwo <ffjlabo@gmail.com>
2020-01-19 14:11:11 +0900

    fix: add css for soft ui

--- src/components/item.js
+++ src/components/item.js
@@ -3,17 +3,31 @@ import styled from "styled-components"
 
 const Item = props => (
   <ItemContainer>
-    <h2>
-      {props.title}
-      <Date>{props.date}</Date>
-    </h2>
-    <p>{props.abstract}</p>
+    <ItemBody>
+      <h2>
+        {props.title}
+        <Date>{props.date}</Date>
+      </h2>
+      <p>{props.abstract}</p>
+    </ItemBody>
   </ItemContainer>
 )
 
 const ItemContainer = styled.div`
   margin: 10px auto;
-  border-bottom: dashed #000000 3px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #f2f1f3;
+  box-shadow: 10px 10px 12px rgba(204, 204, 204, 0.5),
+    -10px -10px 12px rgba(255, 255, 255, 0.5);
+`
+
+const ItemBody = styled.div`
+  background-color: #ffffff;
+  box-shadow: 5px 5px 12px rgba(204, 204, 204, 0.5);
+  padding: 10px;
+  margin: 10px;
 `
 
 const Date = styled.span`
